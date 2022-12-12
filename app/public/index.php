de09edeffa602aca75debc60eb88e97b226f2e0a
drupestre <benoit.croquin@gmail.com>
2022-12-12 15:56:35 +0000

    Add db service.

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,22 @@
 <?php
-echo 'Hello World!';
+echo 'Hello World! hey';
+
+$host = 'db';
+$db   = 'docker-php';
+$user = 'user';
+$pass = 'secret';
+$port = "3306";
+$charset = 'utf8mb4';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+try {
+     $pdo = new \PDO($dsn, $user, $pass, $options);
+     echo "Connection successful";
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
